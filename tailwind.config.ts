d62a9dfb39d62b4afb67d1f01c3c3318f0a3fdd8
M@thieu Ch@mb@ud <mathchambaud2000@gmail.com>
2023-11-29 11:51:42 +0100

    set colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,16 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'text': '#010204',
+      'background': '#ffffff',
+      'primary': '#15c68e',
+      'secondary': '#d1defa',
+      'accent': '#3a599c',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
