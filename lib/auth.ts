81c918f7d3cbe0c3811f60a0eecba933e5592ad7
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-01-17 14:27:51 +0100

    forgot an import fixed

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { options } from "@/app/api/auth/[...nextauth]/route";
+import { options } from "./authOptions";
 import { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
 import { Role } from "./types";
 
