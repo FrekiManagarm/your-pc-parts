04277efc5c657999b02d7bff262b927a1e52f6db
Mathieu Chambaud <mathchambaud2000@gmail.com>
2024-02-23 22:06:36 +0100

    create separate safe actions for admin and mode

--- lib/action.ts
+++ lib/action.ts
@@ -1,9 +1,10 @@
+import { UserRole } from "@prisma/client";
 import { getAuthSession } from "./auth";
 import { createSafeActionClient } from "next-safe-action";
 
 class ServerError extends Error {}
 
-export const authenticatedAction = createSafeActionClient({
+export const adminstratorAction = createSafeActionClient({
   handleReturnedServerError(error) {
     if (error instanceof ServerError) {
       return error.message;
@@ -22,9 +23,40 @@ export const authenticatedAction = createSafeActionClient({
       throw new ServerError("You must be logged in to perform this action");
     }
 
+    if (role !== UserRole.ADMINISTRATOR) {
+      throw new ServerError(
+        "You must be an Administrator to perform this action",
+      );
+    }
+
     return {
       userId,
       user,
     };
+  },
+});
+
+export const moderatorAction = createSafeActionClient({
+  handleReturnedServerError(error) {
+    if (error instanceof ServerError) {
+      return error.message;
+    }
+
+    return error.message;
+  },
+  middleware: async () => {
+    const session = await getAuthSession();
+
+    const user = session?.user;
+    const userId = user?.id;
+    const role = user?.role;
+
+    if (!userId) {
+      throw new ServerError("You must be logged in to perform this action");
+    }
+
+    if (role !== UserRole.MODERATOR) {
+      throw new ServerError("You must a Moderator to perform this action");
+    }
   },
 });
