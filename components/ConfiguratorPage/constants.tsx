8a1a667dcb7a3773add3034220d62188c1f75800
FrekiManagarm <mathchambaud@icloud.com>
2024-02-28 14:20:23 +0100

    wip configurator

---
+++ components/ConfiguratorPage/constants.tsx
@@ -0,0 +1,25 @@
+import { StepDTO } from "react-form-stepper/dist/components/Step/StepTypes";
+
+const steps: StepDTO[] = [
+  {
+    label: "CPU Step",
+  },
+  {
+    label: "GPU Step",
+  },
+  {
+    label: "",
+  },
+  {
+    label: "",
+  },
+  {
+    label: "",
+  },
+  {
+    label: "",
+  },
+  {
+    label: "",
+  },
+]
