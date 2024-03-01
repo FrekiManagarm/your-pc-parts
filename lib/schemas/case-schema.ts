30a704e8fd8a9f139792bb3a236f4ff7e5e9f6e0
FrekiManagarm <mathchambaud@icloud.com>
2024-03-01 14:05:04 +0100

    wip zod schemas

--- lib/schemas/case-schema.ts
+++ lib/schemas/case-schema.ts
@@ -1,5 +1,25 @@
 import { z } from "zod";
 
-export const CaseCreateSchema = z.object({});
+export const CaseCreateSchema = z.object({
+  type: z.string(),
+  color: z.string().nullable(),
+  name: z.string(),
+  imageUrl: z.string().nullable(),
+  included_PSU_W: z.number().nullable(),
+  amazonLink: z.string().nullable(),
+  side_panel: z.string().nullable(),
+  external_volume: z.string().nullable(),
+  internal_35_bays: z.number().nullable(),
+});
 
-export const CaseUpdateSchema = z.object({});
+export const CaseUpdateSchema = z.object({
+  type: z.string(),
+  color: z.string().nullable(),
+  name: z.string(),
+  imageUrl: z.string().nullable(),
+  included_PSU_W: z.number().nullable(),
+  amazonLink: z.string().nullable(),
+  side_panel: z.string().nullable(),
+  external_volume: z.string().nullable(),
+  internal_35_bays: z.number().nullable(),
+});
--- lib/schemas/gpu-schema.ts
+++ lib/schemas/gpu-schema.ts
@@ -12,4 +12,14 @@ export const GPUCreateSchema = z.object({
   length: z.number().nullable(),
 });
 
-export const GPUUpdateSchema = z.object({});
+export const GPUUpdateSchema = z.object({
+  chipset: z.string().nullable(),
+  memory: z.number().nullable(),
+  name: z.string(),
+  imageUrl: z.string().nullable(),
+  amazonLink: z.string().nullable(),
+  core_clock: z.number().nullable(),
+  boost_clock: z.number().nullable(),
+  color: z.string().nullable(),
+  length: z.number().nullable(),
+});
